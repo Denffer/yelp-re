c77e563eef90c3d21f488ccc4c53799266df49ff
denffer <denffer18@gmail.com>
2016-07-28 16:12:16 +0800

    modify get.sh

--- get.sh
+++ get.sh
@@ -1,1 +1,3 @@
-scp cychao@clip2.cs.nccu.edu.tw:/tmp2/cychao/yelp/data/business_list_no_menu.json .
+mkdir data
+sudo pip install beautifulsoup4
+scp cychao@clip2.cs.nccu.edu.tw:/tmp2/cychao/yelp/data/business_list_no_menu.json ./data/
